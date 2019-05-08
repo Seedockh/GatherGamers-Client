a0072eebde5ba5bb6c4e099e9f97c787f5281dad
adrien <adrien.masson@hotmail.fr>
2019-05-08 15:10:00 +0200

    Details Game

--- src/screens/DetailGames/index.js
+++ src/screens/DetailGames/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, AsyncStorage, ScrollView } from 'react-native';
 import { Text, Switch, Button } from 'native-base'
+import ViewMoreText from 'react-native-view-more-text'
 import { vmin } from 'react-native-expo-viewport-units';
 import Style from '../../styles/games'
 import FooterTabs from '../../components/FooterTabs'
@@ -13,7 +14,46 @@ export default class DetailGames extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            token: null,
+            name: null,
+            cover: null,
+            summary: null,
+            text: false
+        }
+    }
+
+    componentDidMount() {
+        this.fetchGames()
+    }
+
+    getToken = async () => {
+        const token = await AsyncStorage.getItem('token');
+        this.setState({ token })
+    }
+
+    fetchGames = async () => {
+        await this.getToken()
+        const url = "https://gathergamers.herokuapp.com/api/game/" + this.props.navigation.state.params.id
+        await fetch(
+            url,
+            {
+                method: "GET",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + this.state.token
+                },
+            }
+        )
+            .then(async (response) => {
+                if (response.status == 401) {
+                    alert("Unauthorized!")
+                } else {
+                    let responseJSON = await response.json()
+                    this.setState({ cover: responseJSON.cover, name: responseJSON.name, summary: responseJSON.summary })
+                }
+            })
     }
 
     onJoinEvent() {
@@ -25,46 +65,48 @@ export default class DetailGames extends React.Component {
     }
 
     onCreateEvent() {
-        this.props.navigation.navigate('CreateEvent', {...this.props})
+        this.props.navigation.navigate('CreateEvent', { ...this.props })
     }
 
     render() {
+        const { cover, summary, text } = this.state
         return (
             <>
-                <View style={{ flex: 1 }}>
-                    <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", margin: 16 }}>
-                        <Image
-                            style={{ width: vmin(20), height: vmin(20), flex: 1 }}
-                            source={require('../../../assets/rouge.jpg')}
-                        />
-                        <View style={{ flex: 2, justifyContent: "center", alignItems: "center", paddingLeft: 8 }}>
-                            <Text>
-                                Horum adventum praedocti speculationibus fidis rectores militum tessera data sollemni armatos omnes celeri eduxere procursu.
-                    </Text>
+                <ScrollView>
+                    <View style={{ flex: 1 }}>
+                        <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+                            <Image
+                                style={{ width: vmin(20), height: vmin(30), flex: 1 }}
+                                source={{ uri: cover }}
+                            />
+                        </View>
+
+                        <View style={{ flex: 2, justifyContent: "center", alignItems: "center", marginHorizontal: 16, marginVertical: 16 }}>
+                            <Text onPress={() => this.setState({text : !this.state.text})} numberOfLines={ text ? null : 5}>{summary}</Text>
+                        </View>
+
+
+                        <View style={{ justifyContent: "space-between", alignItems: "center", flexDirection: "row", marginHorizontal: 16 }}>
+                            <Text>Add Titre du Jeu as favorite</Text>
+                            <Switch value={true} />
+                        </View>
+
+                        <View style={{ marginHorizontal: 16, marginTop: 16 }}>
+                            <Button block style={{ marginVertical: 8, backgroundColor: "black" }} onPress={() => this.onCreateEvent()}>
+                                <Text>Create Event</Text>
+                            </Button>
+                            <Button block style={{ marginVertical: 8, backgroundColor: "black" }} onPress={() => this.onJoinEvent()}>
+                                <Text>Join Event</Text>
+                            </Button>
+                            <Button block style={{ marginVertical: 8, backgroundColor: "black" }} onPress={() => this.onGamersAround()}>
+                                <Text>Check for gamers</Text>
+                            </Button>
+                            <Button block style={{ marginVertical: 8, backgroundColor: "black" }}>
+                                <Text>Forum</Text>
+                            </Button>
                         </View>
                     </View>
-
-                    <View style={{ justifyContent: "space-between", alignItems: "center", flexDirection: "row", marginHorizontal: 16 }}>
-                        <Text>Add Titre du Jeu as favorite</Text>
-                        <Switch value={true} />
-                    </View>
-
-                    <View style={{ marginHorizontal: 16, marginTop: 16 }}>
-                        <Button block style={{ marginVertical: 8, backgroundColor:"black" }} onPress={() => this.onCreateEvent()}>
-                            <Text>Create Event</Text>
-                        </Button>
-                        <Button block style={{ marginVertical: 8, backgroundColor:"black" }} onPress={() => this.onJoinEvent()}>
-                            <Text>Join Event</Text>
-                        </Button>
-                        <Button block style={{ marginVertical: 8, backgroundColor:"black" }} onPress={() => this.onGamersAround()}>
-                            <Text>Check for gamers</Text>
-                        </Button>
-                        <Button block style={{ marginVertical: 8, backgroundColor:"black" }}>
-                            <Text>Forum</Text>
-                        </Button>
-                    </View>
-                </View>
-
+                </ScrollView>
                 <FooterTabs {...this.props} />
             </>
         )
