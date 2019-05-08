955e7d46af7bbeaee081ebf503e93f1da0a44c19
adrien <adrien.masson@hotmail.fr>
2019-05-08 18:17:12 +0200

    Favorite Tabbar

--- src/components/Tabbars/Tabs/tabThree.js
+++ src/components/Tabbars/Tabs/tabThree.js
@@ -1,55 +1,91 @@
 import React from 'react';
-import { ScrollView, View, Image } from 'react-native';
+import { ScrollView, View, Image, AsyncStorage, TouchableOpacity } from 'react-native';
 import { Text } from 'native-base';
-import { vmin } from 'react-native-expo-viewport-units';;
+import { vmin } from 'react-native-expo-viewport-units';
+import JWT from 'expo-jwt'
+import ENV from '../../../../env'
 export default class TabThree extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: [{
-                title: "Apex Legend"
-            },
-            {
-                title: "Sea of Thieves"
-            },
-            {
-                title: "Overwatch"
-            },
-            {
-                title: "Super Smash Bros"
-            }]
+            token: null,
+            gamesFetch: null
         }
+    }
+
+    componentDidMount() {
+        this.fetchFavorite()
+    }
+
+    getToken = async () => {
+        const token = await AsyncStorage.getItem('token');
+        this.setState({ token })
+    }
+
+    fetchFavorite = async () => {
+
+        await this.getToken()
+        let decodedToken = JWT.decode(this.state.token, ENV.JWT_KEY)
+
+        const url = "https://gathergamers.herokuapp.com/api/favourite/user/" + decodedToken.id
+        await fetch(
+            url,
+            {
+                method: "GET",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + this.state.token
+                },
+            }
+        )
+            .then(async (response) => {
+
+                if (response.status == 401) {
+                    alert("Unauthorized!")
+                } else {
+                    let responseJSON = await response.json()
+                    console.log(responseJSON)
+                    this.setState({ gamesFetch: responseJSON.data.favourite.Games })
+                }
+            })
+    }
+
+    onDetails(index) {
+        const { gamesFetch } = this.state
+        this.props.navigation.navigate('DetailGames', {id: gamesFetch[index].id})
     }
 
     render() {
 
-        const { data } = this.state
+        const { gamesFetch } = this.state
 
         return (
             <ScrollView style={{ flex: 1 }}>
 
-                {data.length > 0 ? (
-                    data.map((item, index) => (
+                {gamesFetch ? (
+                    gamesFetch.map((item, index) => (
 
+                        <TouchableOpacity key={index} activeOpacity={0} onPress={() => this.onDetails(index)}>
+                            <View style={{ borderWidth: 3, borderColor: "#000", marginHorizontal: 24, marginVertical: 16, justifyContent: "center", alignItems: "center" }}>
 
-                        <View key={index} style={{ borderWidth: 3, borderColor: "#000", marginHorizontal: 24, marginVertical: 16, justifyContent: "center", alignItems: "center" }}>
+                                <View style={{ paddingBottom: 8, justifyContent: "center", alignItems: "center", backgroundColor: "white", marginTop: -12, paddingHorizontal: 8 }}>
+                                    <Text style={{ fontWeight: "500" }}>{item.name}</Text>
+                                </View>
 
-                            <View style={{ paddingBottom: 8, justifyContent: "center", alignItems: "center", backgroundColor: "white", marginTop: -12, paddingHorizontal: 8 }}>
-                                <Text style={{ fontWeight: "500" }}>{item.title}</Text>
+                                <View style={{ padding: 8 }}>
+                                    <Image
+                                        style={{ width: vmin(20), height: vmin(20) }}
+                                        source={{ uri: item.cover }}
+                                    />
+                                </View>
+
                             </View>
-
-                            <View style={{ padding: 8 }}>
-                                <Image
-                                    style={{ width: vmin(20), height: vmin(20) }}
-                                    source={require('../../../../assets/logo.png')}
-                                />
-                            </View>
-
-                        </View>
+                        </TouchableOpacity>
 
                     ))
                 ) : (
-                        <Text>Pas de news</Text>
+                        <Text>You have no favorites</Text>
                     )}
 
             </ScrollView>
