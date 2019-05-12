072412039c34a6141bd7527cd6bf894af72bc9d2
Adrien <adrien.masson@hotmail.fr>
2019-05-12 11:04:53 +0200

    joinevent

--- src/screens/JoinEvent/index.js
+++ src/screens/JoinEvent/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { View, TouchableOpacity, ActivityIndicator } from 'react-native';
-import { Header, Item, Input, Icon, Text, List, ListItem, Thumbnail, Left, Body } from 'native-base';
-import { vmin } from 'react-native-expo-viewport-units';
+import { Header, Item, Input, Icon, Text, List, ListItem, Body, Right } from 'native-base';
 import FooterTabs from '../../components/FooterTabs'
 import { ScrollView } from 'react-native-gesture-handler';
 import { MapView, Location, Permissions } from 'expo';
@@ -10,226 +9,230 @@ import JWT from 'expo-jwt'
 import KEY from '../../../secretenv.js'
 import Func from '../../functions.js';
 import geolib from 'geolib';
+import Style from '../../styles/joinevent'
 
 const events = []
 
 export default class JoinEvents extends React.Component {
-    static navigationOptions = {
-        headerTitle: "Join Event"
+  static navigationOptions = {
+    headerTitle: "Join Event"
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: "",
+      eventsCount: 0,
+      token: null,
+      allowGeoloc: false,
+      location: {
+        type: 'Point',
+        coordinates: [48, 2]
+      },
+      locationResult: false,
+      fetchDone: false,
     }
-    constructor(props) {
-        super(props);
-        this.state = {
-            searchText: "",
-            eventsCount: 0,
-            token: null,
-            allowGeoloc: false,
-            location: {
-              type: 'Point',
-              coordinates: [48,2]
-            },
-            locationResult: false,
-            fetchDone: false,
-        }
-        events.length = 0;
-    }
+    events.length = 0;
+  }
 
-    async componentDidMount() {
-        await this.checkGeolocation();
-        if (this.state.allowGeoloc) await this.getUserLocation();
-        await this.fetchEvents();
-    }
+  async componentDidMount() {
+    await this.checkGeolocation();
+    if (this.state.allowGeoloc) await this.getUserLocation();
+    await this.fetchEvents();
+  }
 
-    async checkGeolocation() {
-      // permissions returns only for location permissions on iOS and under certain conditions, see Permissions.LOCATION
-      const { status, permissions } = await Permissions.askAsync(Permissions.LOCATION);
-      if (status === 'granted') {
-        this.setState({ allowGeoloc: true })
-        return Location.getCurrentPositionAsync({enableHighAccuracy: true});
-      } else {
-        this.setState({ allowGeoloc: false })
-        Func.toaster("Location permission not granted!", "Okay", "danger", 3000);
-      }
+  async checkGeolocation() {
+    // permissions returns only for location permissions on iOS and under certain conditions, see Permissions.LOCATION
+    const { status, permissions } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status === 'granted') {
+      this.setState({ allowGeoloc: true })
+      return Location.getCurrentPositionAsync({ enableHighAccuracy: true });
+    } else {
+      this.setState({ allowGeoloc: false })
+      Func.toaster("Location permission not granted!", "Okay", "danger", 3000);
     }
+  }
 
-    fetchEvents = async () => {
-        const token = await Func.getToken()
-        this.setState({ token })
-        const url = "https://gathergamers.herokuapp.com/api/event/game/"+this.props.navigation.state.params.gameid ;
-        const auth = `Bearer ${token}`
-        const response = await Func.fetch(url, "GET", null, auth)
-        if(response.status == 401) {
-          Func.toaster("Unauthorized!", "Okay", "danger", 3000);
-        } else {
-            let responseJSON = await response.json()
-            // When game's events are fetched
-              responseJSON.map(async (event) => {
-                // Get distance from current user
-               const distFromUser = this.state.allowGeoloc ?
-                  await geolib.getDistanceSimple(
-                    {latitude: this.state.location.coordinates[0], longitude: this.state.location.coordinates[1]},
-                    {latitude: event.place.coordinates[0], longitude: event.place.coordinates[1]}
-                  ) / 1000 : null;
-               // Get address string from event coordinates
-               const address =
-               await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${event.place.coordinates[0]},${event.place.coordinates[1]}&key=${KEY.MAPS_API_KEY}`)
-                     .then(res => res.json())
-                     .then((json) => {
-                       if (json.status !== 'OK') {
-                         throw new Error(`Geocode error: ${json.status}`);
-                       }
-                     return json.results[0].formatted_address;
-                   });
-              // Push the event with all infos
-              const eventToPush = {
-                id: event.id,
-                title: event.name,
-                date: event.date,
-                place: event.place,
-                address: address,
-                distance: distFromUser,
-                gameid: event.GameId,
-                players: event.players,
-                price: event.price,
-                type: event.type,
-                user: event.UserId
+  fetchEvents = async () => {
+    const token = await Func.getToken()
+    this.setState({ token })
+    const url = "https://gathergamers.herokuapp.com/api/event/game/" + this.props.navigation.state.params.gameid;
+    const auth = `Bearer ${token}`
+    const response = await Func.fetch(url, "GET", null, auth)
+    if (response.status == 401) {
+      Func.toaster("Unauthorized!", "Okay", "danger", 3000);
+    } else {
+      let responseJSON = await response.json()
+      // When game's events are fetched
+      responseJSON.map(async (event) => {
+        // Get distance from current user
+        const distFromUser = this.state.allowGeoloc ?
+          await geolib.getDistanceSimple(
+            { latitude: this.state.location.coordinates[0], longitude: this.state.location.coordinates[1] },
+            { latitude: event.place.coordinates[0], longitude: event.place.coordinates[1] }
+          ) / 1000 : null;
+        // Get address string from event coordinates
+        const address =
+          await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${event.place.coordinates[0]},${event.place.coordinates[1]}&key=${KEY.MAPS_API_KEY}`)
+            .then(res => res.json())
+            .then((json) => {
+              if (json.status !== 'OK') {
+                throw new Error(`Geocode error: ${json.status}`);
               }
-              events.push(eventToPush)
-              if (events.length===responseJSON.length) this.setState({fetchDone: true});
+              return json.results[0].formatted_address;
             });
+        // Push the event with all infos
+        const eventToPush = {
+          id: event.id,
+          title: event.name,
+          date: event.date,
+          place: event.place,
+          address: address,
+          distance: distFromUser,
+          gameid: event.GameId,
+          players: event.players,
+          price: event.price,
+          type: event.type,
+          user: event.UserId
         }
+        events.push(eventToPush)
+        if (events.length === responseJSON.length) this.setState({ fetchDone: true });
+      });
     }
+  }
 
-    // Checks if user has Accepted Geolocation and returns his position to DB
-    async getUserLocation() {
-      if (await Location.hasServicesEnabledAsync()) {
-        let location = await Location.getCurrentPositionAsync({accuracy: 2});
-        await this.setState({ location: {
+  // Checks if user has Accepted Geolocation and returns his position to DB
+  async getUserLocation() {
+    if (await Location.hasServicesEnabledAsync()) {
+      let location = await Location.getCurrentPositionAsync({ accuracy: 2 });
+      await this.setState({
+        location: {
           type: 'Point',
-          coordinates: [location.coords.latitude,location.coords.longitude]
-        }, locationResult: true })
-
-        // Sending last User Location to DB
-        this.updateUserLocation();
-      } else {
-        this.state({ locationResult: 'Location permission not enabled !'});
-      }
-    }
-
-    async updateUserLocation() {
-      const token = await Func.getToken()
-      this.setState({ token })
-      const { location } = this.state
-      let decodedToken = await JWT.decode(token, ENV.JWT_KEY)
-      const url = `https://gathergamers.herokuapp.com/api/user/updatelocation/${decodedToken.id}`
-      const body = JSON.stringify({
-        latitude: location.coordinates[0],
-        longitude: location.coordinates[1],
+          coordinates: [location.coords.latitude, location.coords.longitude]
+        }, locationResult: true
       })
-      const auth = `Bearer ${token}`
-      const response = await Func.fetch(url, 'PUT', body, auth)
-      if (response.status == 401) {
-        Func.toaster("Unauthorized!", "Okay", "danger", 3000);
-      } else {
-        let responseJSON = await response.json();
-        return responseJSON;
-      }
-    }
 
-    handleMapRegionChange = mapRegion => {
-      this.setState({ mapRegion });
-    };
-
-    getDetails(index) {
-        this.props.navigation.navigate('DetailEvents', {event: events[index], userLocation: this.state.location})
+      // Sending last User Location to DB
+      this.updateUserLocation();
+    } else {
+      this.state({ locationResult: 'Location permission not enabled !' });
     }
+  }
 
-    renderMarker(item,index) {
-      return(
-        <MapView.Marker key={index}
-          coordinate={{
-            latitude: item.place.coordinates[0],
-            longitude: item.place.coordinates[1]
-          }}
-          title={item.title}
-          description={item.address}
-          pinColor={ 'blue' }
-        />
-      )
+  async updateUserLocation() {
+    const token = await Func.getToken()
+    this.setState({ token })
+    const { location } = this.state
+    let decodedToken = await JWT.decode(token, ENV.JWT_KEY)
+    const url = `https://gathergamers.herokuapp.com/api/user/updatelocation/${decodedToken.id}`
+    const body = JSON.stringify({
+      latitude: location.coordinates[0],
+      longitude: location.coordinates[1],
+    })
+    const auth = `Bearer ${token}`
+    const response = await Func.fetch(url, 'PUT', body, auth)
+    if (response.status == 401) {
+      Func.toaster("Unauthorized!", "Okay", "danger", 3000);
+    } else {
+      let responseJSON = await response.json();
+      return responseJSON;
     }
+  }
 
-    renderItem(item, index) {
-      if (item.title.toLowerCase().indexOf(this.state.searchText.toLowerCase()) !== -1) {
-      return(
-          <List key={index} >
-              <ListItem thumbnail>
-              <TouchableOpacity key={index} activeOpacity={0.8} style={{flexDirection : "row"}} onPress={() => this.getDetails(index)}>
-                  <Left>
-                      <Thumbnail square source={require('../../../assets/rouge.jpg')} />
-                  </Left>
-                  <Body>
-                      <Text>{item.title}</Text>
-                      <Text note numberOfLines={1}>{item.date}</Text>
-                      <Text note numberOfLines={1}>{item.address}</Text>
-                      <Text note numberOfLines={1}>Distance : {item.distance}km</Text>
-                  </Body>
-              </TouchableOpacity>
-              </ListItem>
-          </List>)
-      } else return null;
-    }
+  handleMapRegionChange = mapRegion => {
+    this.setState({ mapRegion });
+  };
 
-    render() {
-        return (
-            <>
-              {this.state.allowGeoloc && !this.state.locationResult &&
-                <View style={{ flex: 1, justifyContent: 'center', flexDirection: 'row'}}>
-                  <ActivityIndicator style={{justifyContent: 'space-around', padding: 0}} size="large" color="#000000" />
-                </View>
-              }
-              {this.state.locationResult &&
-                <MapView style={{ width: vmin(20), height: vmin(30), flex: 1 }}
-                    style={{ flex: 1 }}
-                    initialRegion={{
-                      latitude: this.state.location.coordinates[0],
-                      longitude: this.state.location.coordinates[1],
-                      latitudeDelta: 0.0922,
-                      longitudeDelta: 0.0421,
-                    }}
-                  >
-                  {this.state.allowGeoloc &&
-                    <MapView.Marker
-                      coordinate={{
-                        latitude: this.state.location.coordinates[0],
-                        longitude: this.state.location.coordinates[1]
-                      }}
-                      title="Your position"
-                      description="This where you are actually located."
-                    />
-                  }
-                  {this.state.fetchDone ? events.map((item,index)=>this.renderMarker(item,index)) : null}
-                </MapView>
-              }
-                <View style={{ flex: 1 }}>
-                    <Header searchBar rounded>
-                        <Item>
-                            <Icon name="ios-search" />
-                            <Input placeholder="Search" onChangeText={(searchText) => this.setState({searchText})} />
-                        </Item>
-                    </Header>
-                    {!this.state.fetchDone && (
-                      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row'}}>
-                        <ActivityIndicator style={{justifyContent: 'space-around', padding: 0}} size="large" color="#000000" />
-                      </View>
-                    )}
-                    {this.state.fetchDone && (
-                      <ScrollView>
-                          {events.map((item, index) => this.renderItem(item, index))}
-                      </ScrollView>
-                    )}
-                </View>
-                <FooterTabs {...this.props} />
-            </>
-        )
-    }
+  getDetails(index) {
+    this.props.navigation.navigate('DetailEvents', { event: events[index], userLocation: this.state.location })
+  }
+
+  renderMarker(item, index) {
+    return (
+      <MapView.Marker key={index}
+        coordinate={{
+          latitude: item.place.coordinates[0],
+          longitude: item.place.coordinates[1]
+        }}
+        title={item.title}
+        description={item.address}
+        pinColor={'blue'}
+      />
+    )
+  }
+
+  renderItem(item, index) {
+    if (item.title.toLowerCase().indexOf(this.state.searchText.toLowerCase()) !== -1) {
+      return (
+        <List key={index} >
+          <ListItem >
+            <TouchableOpacity key={index} activeOpacity={0.8} style={Style.touchable} onPress={() => this.getDetails(index)}>
+              <Body>
+                <Text>{item.title}</Text>
+                <Text note numberOfLines={1}>{item.date}</Text>
+                <Text note numberOfLines={1}>{item.address}</Text>
+                <Text note numberOfLines={1}>Distance : {item.distance}km</Text>
+              </Body>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </TouchableOpacity>
+          </ListItem>
+        </List>)
+    } else return null;
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.allowGeoloc && !this.state.locationResult &&
+          <View style={Style.activityview}>
+            <ActivityIndicator style={Style.activity} size="large" color="#000000" />
+          </View>
+        }
+        {this.state.locationResult &&
+          <MapView 
+            style={{flex:1}}
+            style={Style.mapview}
+            initialRegion={{
+              latitude: this.state.location.coordinates[0],
+              longitude: this.state.location.coordinates[1],
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          >
+            {this.state.allowGeoloc &&
+              <MapView.Marker
+                coordinate={{
+                  latitude: this.state.location.coordinates[0],
+                  longitude: this.state.location.coordinates[1]
+                }}
+                title="Your position"
+                description="This where you are actually located."
+              />
+            }
+            {this.state.fetchDone ? events.map((item, index) => this.renderMarker(item, index)) : null}
+          </MapView>
+        }
+        <View style={Style.view}>
+          <Header searchBar rounded style={Style.header}>
+            <Item>
+              <Icon name="ios-search" />
+              <Input placeholder="Search" onChangeText={(searchText) => this.setState({ searchText })} />
+            </Item>
+          </Header>
+          {!this.state.fetchDone && (
+            <View style={Style.activityview}>
+              <ActivityIndicator style={Style.activity} size="large" color="#000000" />
+            </View>
+          )}
+          {this.state.fetchDone && (
+            <ScrollView>
+              {events.map((item, index) => this.renderItem(item, index))}
+            </ScrollView>
+          )}
+        </View>
+        <FooterTabs {...this.props} />
+      </>
+    )
+  }
 }
